4e6a2933187d6e040faf27f1d634718c59b3db0d
sscnt <pixeltc@gmail.com>
2014-04-17 15:38:27 +0900

    Initial Commit

---
+++ Vintage 2.0/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Vintage 2.0
+//
+//  Created by SSC on 2014/04/17.
+//  Copyright (c) 2014年 SSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Vintage 2.0/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Vintage 2.0
+//
+//  Created by SSC on 2014/04/17.
+//  Copyright (c) 2014年 SSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
