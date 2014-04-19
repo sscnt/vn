cc60626d2f50bd2daf3eea6427d7826b48691247
sscnt <pixeltc@gmail.com>
2014-04-20 00:52:01 +0900

    a

---
+++ Vintage 2.0/VnViewControllerRoot.h
@@ -0,0 +1,13 @@
+//
+//  VnViewControllerRoot.h
+//  Vintage 2.0
+//
+//  Created by SSC on 2014/04/20.
+//  Copyright (c) 2014年 SSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VnViewControllerRoot : UINavigationController
+
+@end
