8977bbddac67ff79d60c00ef27ca2815e72f6ce2
sscnt <pixeltc@gmail.com>
2014-04-19 18:38:44 +0900

    bl

--- Vintage 2.0/VnEffect.h
+++ Vintage 2.0/VnEffect.h
@@ -9,65 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "GPUImage.h"
 
-typedef NS_ENUM(NSInteger, VnEffectId){
-    VnEffectIdHaze3 = 1,
-    VnEffectIdHaze3Pink,
-    VnEffectIdHazelnut,
-    VnEffectIdHazelnutPink,
-    VnEffectIdVintageFilm,
-    VnEffectIdVintage2,
-    VnEffectIdVintage1,
-    VnEffectIdOldTone,
-    VnEffectIdWeekend,
-    VnEffectIdWarmAutumn,
-    VnEffectIdJoyful,
-    VnEffectIdWarmSpringLight,
-    VnEffectIdGentleMemories,
-    VnEffectIdCreamyNoon,
-    VnEffectIdVividVintage,
-    VnEffectIdCavalleriaRusticana,
-    VnEffectIdDreamyVintage,
-    VnEffectIdSunsetCarnevale,
-    VnEffectIdBeachVintage,
-    VnEffectIdPinkBubbleTea,
-    VnEffectIdFaerieVintage,
-    VnEffectIdGirder,
-    VnEffectIdSummers,
-    VnEffectIdMiami,
-    VnEffectIdVampire,
-    VnEffectIdAutumnVintage,
-    VnEffectIdBokehileVintage,
-    VnEffectIdBokehileVintage2,
-    VnEffectIdGoodMorning,
-    VnEffectIdVintageBaby,
-    VnEffectIdDreamyCreamy
-};
-
-typedef NS_ENUM(NSInteger, VnBlendingMode){
-    VnBlendingModeNormal = 1,
-    VnBlendingModeDarken,
-    VnBlendingModeScreen,
-    VnBlendingModeMultiply,
-    VnBlendingModeDarkerColor,
-    VnBlendingModeLighten,
-    VnBlendingModeSoftLight,
-    VnBlendingModeHardLight,
-    VnBlendingModeVividLight,
-    VnBlendingModeOverlay,
-    VnBlendingModeExclusion,
-    VnBlendingModeColorBurn,
-    VnBlendingModeColor,
-    VnBlendingModeColorDodge,
-    VnBlendingModeLinearDodge,
-    VnBlendingModeLinearLight,
-    VnBlendingModeHue,
-    VnBlendingModeSaturation,
-    VnBlendingModeDifference
-};
-
-
 @interface VnEffect : NSObject
-
 
 @property (nonatomic, weak) UIImage* imageToProcess;
 @property (nonatomic, assign) VnEffectId effectId;
